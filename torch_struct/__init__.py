160cf03b4a5d41e3912ebfab94ce7a35cf7f701c
Sasha <arush@g.harvard.edu>
2019-08-27 21:30:01 -0400

    .

--- torch_struct/__init__.py
+++ torch_struct/__init__.py
@@ -1,6 +1,14 @@
-from .algorithms import linearchain
+from .cky import cky_inside, cky
+from .deptree import deptree_inside, deptree
+from .linearchain import  linearchain, linearchain_inside
+from .semimarkov import  semimarkov, semimarkov_inside
+from .semirings import LogSemiring, StdSemiring, SampledSemiring, MaxSemiring
 
 version = "0.0.1"
 
 # For flake8 compatibility.
-__all__ = [linearchain]
+__all__ = [linearchain, linearchain_inside,
+           semimarkov, semimarkov_inside,
+           cky_inside, cky,
+           deptree_inside, deptree,
+           LogSemiring, StdSemiring, SampledSemiring, MaxSemiring]
--- torch_struct/test_algorithms.py
+++ torch_struct/test_algorithms.py
@@ -1,5 +1,7 @@
-from .algorithms import linearchain, linearchain_inside, linearchain_check, dependencytree_inside
-
+from .cky import cky_inside, cky, cky_check
+from .deptree import deptree_inside, deptree, deptree_check
+from .linearchain import  linearchain, linearchain_inside, linearchain_check
+from .semimarkov import  semimarkov, semimarkov_inside, semimarkov_check
 from .semirings import LogSemiring, StdSemiring
 import torch
 import math
@@ -14,17 +16,58 @@ from hypothesis.strategies import (
 )
 
 smint = integers(min_value=2, max_value=5)
-@given(smint, smint, smint)
-def test_chain(batch, n, c):
-    total_paths = math.pow(c, n + 1)
-    alpha = linearchain_inside(torch.zeros(batch, n, c, c).exp().float(),
-                        semiring=StdSemiring)
-    count = linearchain_check(torch.zeros(batch, n, c, c).exp().float(),
-                        semiring=StdSemiring)
-    print(count.shape, alpha[n].shape)
-    assert(torch.isclose(StdSemiring.sum(alpha[n]), count).all())
+# @given(smint, smint, smint)
+def test_linearchain():
+    batch, N, C = 2, 3, 4
+    # vals = torch.ones(batch, n, c, c)
+    # semiring = StdSemiring
+    vals = torch.rand(batch, N, C, C)
+    semiring = LogSemiring
+
+    alpha = linearchain_inside(vals, semiring)
+    count = linearchain_check(vals, semiring)
+    print(count.shape, alpha.shape)
+    assert(torch.isclose(count[0], alpha[0]))
 
 
-@given(smint, smint, smint)
-def test_dep(batch, n, c):
-    pass
+smint = integers(min_value=2, max_value=5)
+def test_semimarkov():
+    batch = 2
+    N, K, C = 4, 3, 5
+    # vals = torch.ones(batch, n, K, C, C)
+    # semiring = StdSemiring
+    vals = torch.rand(batch, N, K, C, C)
+    semiring = LogSemiring
+    vals[:, :, 0, :, :] = semiring.zero()
+    v = semimarkov_inside(vals, semiring)
+    count = semimarkov_check(vals, semiring)
+    assert(torch.isclose(v, count).all())
+
+
+
+@given(smint, smint)
+def test_dep(batch, N):
+    N = 5
+    batch = 2
+
+    scores = torch.rand(batch, N, N)
+    semiring = LogSemiring
+
+    top, arcs = deptree_inside(scores, semiring)
+    out = deptree_check(scores, semiring)
+    assert(torch.isclose(top[0], out))
+
+
+def test_cky():
+    N = 3
+    batch = 1
+    NT = 4
+    T = 5
+    terms = torch.rand(batch, N, T)
+    rules = torch.rand(batch, NT, (NT+T), (NT+T))
+    roots = torch.rand(batch, NT)
+    semiring = LogSemiring
+
+    v2 = cky_check(terms, rules, roots, semiring)
+    v, _ = cky_inside(terms, rules, roots, semiring)
+    assert(torch.isclose(v[0], v2))
