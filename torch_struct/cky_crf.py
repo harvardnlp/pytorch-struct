830fc20c814f5b2ffb06a932304213ce99f0ff1b
srush <srush.research@gmail.com>
2019-09-16 14:16:55 +0000

    .

--- torch_struct/cky_crf.py
+++ torch_struct/cky_crf.py
@@ -27,13 +27,13 @@ class CKY_CRF(_Struct):
         beta[A][:, :, ns, 0] = rule_use[0]
         beta[B][:, :, ns, N - 1] = rule_use[0]
         for w in range(1, N):
-            Y = beta[A][:, :, : N - w, :w].view(ssize, batch, N - w, w, 1, NT, 1)
-            Z = beta[B][:, :, w:, N - w :].view(ssize, batch, N - w, w, 1, 1, NT)
+            Y = beta[A][:, :, : N - w, :w].view(ssize, batch, N - w, 1, w,  NT, 1)
+            Z = beta[B][:, :, w:, N - w :].view(ssize, batch, N - w, 1, w,  1, NT)
             f = torch.arange(N-w), torch.arange(w, N)
             X = scores[:, :, f[0], f[1]].view(ssize, batch, N-w, NT)
-
+            merge = semiring.times(Y, Z).view(ssize, batch, N - w, -1)
             rule_use[w ][:] = semiring.times(
-                semiring.sum(semiring.sum(semiring.sum(semiring.times(Y, Z))), dim=3), X
+                semiring.sum(merge, X)
             )
             span[w] = rule_use[w].view(ssize, batch, N - w, NT)
             beta[A][:, :, : N - w, w] = span[w]
@@ -43,7 +43,7 @@ class CKY_CRF(_Struct):
         log_Z =  torch.stack(
             [final[:, b,  l - 1] for b, l in enumerate(lengths)], dim=1
         )
-        return semiring.unconvert(log_Z), rule_use, beta
+        return log_Z, rule_use, beta
 
     def _arrange_marginals(self, grads):
         semiring = self.semiring
