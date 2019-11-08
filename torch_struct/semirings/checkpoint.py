c850185435cfca14cd5255d16158dd7d221ef305
Sasha <arush@g.harvard.edu>
2019-11-08 17:47:56 -0500

    .

--- torch_struct/semirings/checkpoint.py
+++ torch_struct/semirings/checkpoint.py
@@ -18,7 +18,7 @@ def CheckpointSemiring(cls,  max_size, min_size=0):
             a, b = ctx.saved_tensors
             size = [max(p, q) for p, q in zip(a.shape, b.shape)][:-1]
             fn = lambda a, b: cls.dot(a, b)
-            grad_a, grad_b = unaccumulate2_(
+            grad_a, grad_b = unaccumulate_(
                 a, b, grad_output, len(grad_output.shape), fn,
                 step=max_size // a.shape[-1] + 2
             )
@@ -60,34 +60,62 @@ def accumulate_(a, b, size, fn, preserve, step=10000):
 
 
     ret = torch.zeros(*size, dtype=a.dtype, device=a.device)
-    a_one, b_one = ones(a), ones(b)
 
-    print(a.shape, b.shape)
     a = a.expand(*size[:-2], 1,  a.shape[-2], a.shape[-1])
     b = b.expand(*size[:-2], b.shape[-3], 1,  b.shape[-1])
 
     a2 = a.contiguous().view(-1, 1, a.shape[-2], a.shape[-1])
     b2 = b.contiguous().view(-1, b.shape[-3], 1, b.shape[-1])
     ret = ret.view(-1, b.shape[-3], a.shape[-2])
-
     for p in range(0, ret.shape[0], step):
-        # ind = indices[:, p : p + step].unbind()
-        # a_ind = mind(a_one, ind)
-        # b_ind = mind(b_one, ind)
         ret[p:p+step, :] = fn(a2[p:p+step], b2[p:p+step])
     ret = ret.view(*size)
-    # t = time()
-    # indices = torch.tensor(np.mgrid[slices]).view(len(ret.shape[:preserve]), -1)
-    # print("trigger", step, total, time() - t)
-
-    # t = time()
-    # for p in range(0, total, step):
-    #     ind = indices[:, p : p + step].unbind()
-    #     a_ind = mind(a_one, ind)
-    #     b_ind = mind(b_one, ind)
-    #     ret[ind] = fn(a[tuple(a_ind)], b[tuple(b_ind)])
-    # print("done", time() - t)
     return ret
+
+
+def unaccumulate_(a, b, grad_output, preserve, fn, step=10000):
+    slices = []
+
+    total = 1
+    size = grad_output.shape[:preserve]
+    for s in grad_output.shape[:preserve]:
+        slices.append(slice(s))
+        total *= s
+
+    if step > total:
+        with torch.enable_grad():
+            a_in = a.clone().requires_grad_(True)
+            b_in = b.clone().requires_grad_(True)
+            q = fn(a, b)
+        ag, bg = torch.autograd.grad(q, (a, b), grad_output)
+        return ag, bg
+
+    a2 = a.expand(*size[:-2], 1,  a.shape[-2], a.shape[-1])
+    b2 = b.expand(*size[:-2], b.shape[-3], 1,  b.shape[-1])
+    a2 = a2.contiguous().view(-1, 1, a2.shape[-2], a2.shape[-1])
+    b2 = b2.contiguous().view(-1, b2.shape[-3], 1, b2.shape[-1])
+
+    a_grad = a2.clone().fill_(0)
+    b_grad = b2.clone().fill_(0)
+
+    grad_output = grad_output.view(-1, b.shape[-3], a.shape[-2])
+    for p in range(0, total, step):
+        with torch.enable_grad():
+            a_in = a2[p:p+step].clone().requires_grad_(True)
+            b_in = b2[p:p+step].clone().requires_grad_(True)
+            q = fn(a_in, b_in)
+        ag, bg = torch.autograd.grad(q, (a_in, b_in), grad_output[p:p+step])
+        a_grad[p:p+step] += ag
+        b_grad[p:p+step] += bg
+
+    a_grad = a_grad.view(*size[:-2], 1,  a.shape[-2], a.shape[-1])
+    b_grad = b_grad.view(*size[:-2], b.shape[-3],  1, a.shape[-1])
+    a_ones = ones(a)
+    b_ones = ones(b)
+
+    f1, f2 = a_grad.sum(a_ones, keepdim=True), b_grad.sum(b_ones, keepdim=True)
+    return f1, f2
+
 
 # def unaccumulate_(a, b, grad_output, fn, step=10000):
 #     slices = []
@@ -111,40 +139,3 @@ def accumulate_(a, b, size, fn, preserve, step=10000):
 #         a_grad.index_put_(tuple(a_ind),  q, accumulate=True)
 #         b_grad.index_put_(tuple(b_ind),  q, accumulate=True)
 #     return a_grad, b_grad
-
-def unaccumulate2_(a, b, grad_output, preserve, fn, step=10000):
-    slices = []
-    a_grad = a.clone().fill_(0)
-    b_grad = b.clone().fill_(0)
-
-    total = 1
-    for s in grad_output.shape[:preserve]:
-        slices.append(slice(s))
-        total *= s
-
-    if step > total:
-        with torch.enable_grad():
-            a_in = a.clone().requires_grad_(True)
-            b_in = b.clone().requires_grad_(True)
-            q = fn(a, b)
-        ag, bg = torch.autograd.grad(q, (a, b), grad_output)
-        return ag, bg
-
-    a_one, b_one = ones(a), ones(b)
-    print(a.shape, b.shape, a_one, b_one, preserve)
-    indices = torch.tensor(np.mgrid[slices]).view(len(grad_output.shape[:preserve]), -1)
-
-    for p in range(0, total, step):
-        ind = indices[:, p : p + step].unbind()
-        a_ind = mind(a_one, ind)
-        b_ind = mind(b_one, ind)
-
-        with torch.enable_grad():
-            a_in = a.clone().requires_grad_(True)
-            b_in = b.clone().requires_grad_(True)
-            q = fn(a[tuple(a_ind)], b[tuple(b_ind)])
-        ag, bg = torch.autograd.grad(q, (a, b), grad_output[tuple(ind)])
-        a_grad += ag
-        b_grad += bg
-
-    return a_grad, b_grad
