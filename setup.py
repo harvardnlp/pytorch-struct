926b94cfb0e4e21d0dc3a87cb998fc7adf1b9310
srush <srush.research@gmail.com>
2021-02-13 21:34:46 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="torch_struct",
     version="0.5",
@@ -9,9 +12,12 @@ setup(
         "torch_struct",
         "torch_struct.semirings",
     ],
+    long_description=long_description,
     package_data={"torch_struct": []},
+    long_description_content_type="text/markdown",
     url="https://github.com/harvardnlp/pytorch_struct",
     install_requires=["torch"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
+    python_requires='>=3.6',
 )
