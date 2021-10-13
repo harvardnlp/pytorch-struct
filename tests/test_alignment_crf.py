7d140f639f270102de8a88e4edd32132b9911698
John Reid <john.reid@blueprism.com>
2021-10-13 07:54:51 +0000

    Update test to use skipif

--- tests/test_alignment_crf.py
+++ tests/test_alignment_crf.py
@@ -1,30 +1,25 @@
 import torch
 import torch_struct
 import warnings
+import pytest
 
 
+@pytest.skipif(not torch.cuda.isavailable(), 'needs CUDA')
 def test_alignment_crf_shapes():
     batch, N, M = 2, 4, 5
-    log_potentials = torch.rand(batch, N, M, 3)
-
-    if torch.cuda.is_available():
-        log_potentials = log_potentials.cuda()
-    else:
-        warnings.warn('Could not move log potentials to CUDA device. '
-                      'Will not test marginals.')
+    log_potentials = torch.rand(batch, N, M, 3).cuda()
 
     dist = torch_struct.AlignmentCRF(log_potentials)
     assert (batch, N, M, 3) == dist.argmax.shape
-    if torch.cuda.is_available():
-        assert (batch, N, M, 3) == dist.marginals.shape
-        assert (batch,) == dist.partition.shape
+    assert (batch, N, M, 3) == dist.marginals.shape
+    assert (batch,) == dist.partition.shape
 
-        # Fail due to AttributeError: 'BandedMatrix' object has no attribute
-        # 'unsqueeze'
-        # assert (batch,) == dist.entropy.shape
-        # assert (9, batch, N, M, 3) == dist.sample([9]).shape
+    # Fail due to AttributeError: 'BandedMatrix' object has no attribute
+    # 'unsqueeze'
+    assert (batch,) == dist.entropy.shape
+    assert (9, batch, N, M, 3) == dist.sample([9]).shape
 
-        # Fails due to: RuntimeError: Expected condition, x and y to be on
-        # the same device, but condition is on cpu and x and y are on
-        # cuda:0 and cuda:0 respectively
-        # assert (8, batch,) == dist.topk(8).shape
+    # Fails due to: RuntimeError: Expected condition, x and y to be on
+    # the same device, but condition is on cpu and x and y are on
+    # cuda:0 and cuda:0 respectively
+    assert (8, batch,) == dist.topk(8).shape
